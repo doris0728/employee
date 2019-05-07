71c1ad00a5dd1c0a7ad883ec592f2be186096c5a
jjtse <vivi999vivi999@gmail.com>
2019-05-08 00:24:04 +0800

    Update ReserveTable.jsx

--- src/components/reserve/ReserveTable.jsx
+++ src/components/reserve/ReserveTable.jsx
@@ -216,9 +216,7 @@ class SimpleTable extends React.Component{
               <TableCell align="center" style={{color:'#969696',fontFamily: "Microsoft JhengHei",
                         letterSpacing:4,fontSize:15,fontWeight: "bold"}}>日期</TableCell>
               <TableCell align="center" style={{color:'#969696',fontFamily: "Microsoft JhengHei",
-                        letterSpacing:4,fontSize:15,fontWeight: "bold"}}>校區</TableCell>
               <TableCell align="center" style={{color:'#969696',fontFamily: "Microsoft JhengHei",
-                        letterSpacing:4,fontSize:15,fontWeight: "bold"}}>補課時間</TableCell>
               <TableCell align="center" style={{color:'#969696',fontFamily: "Microsoft JhengHei",
                         letterSpacing:4,fontSize:15,fontWeight: "bold"}}>人數</TableCell>
               <TableCell align="center" style={{color:'#969696',fontFamily: "Microsoft JhengHei",
