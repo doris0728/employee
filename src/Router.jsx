4f563aa93ca887cb5effc9e6cf573c5f614281df
kelly870114 <kelly87011431@gmail.com>
2019-05-20 21:45:36 +0800

    Update Router.jsx

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,8 +4,6 @@ import './index.css';
 import ReactApp from './components/react-app.jsx';
 import * as serviceWorker from './serviceWorker';
 import Bar from './components/Bar.jsx'
-import Try from './components/tabletry.jsx'
-import Reserve from './components/reserve/reserveComponent.jsx'
 import Login from './components/Loginpage/login.jsx';
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import { withStyles, MuiThemeProvider, createMuiTheme,styles } from '@material-ui/core/styles';
@@ -16,22 +14,24 @@ import ReserveComponent from './components/reserve/reserveComponent';
 import Reserve2 from './components/reserve2/reserveComponent2';
 import Reserve3 from './components/reserveList/reserveListComponent';
 import MyPage from './components/studentpage/studentpage';
+import theme from './theme';
+import Theme from '@e-group/react-material-components/dist/Theme';
 
 //ReactDOM.render(<Index />, document.getElementById('root'));
 class Barrr extends React.Component {
     render() {
         return(
             <Router>
-            <div>
-                <Route path="/" component={Login} />
-                <Route path="/index" component={Bar} />
-                <Route path="/attend" component={AttendComponent} />
-                <Route path="/score" component={ScoreComponent} />
-                <Route path="/reserve" component={ReserveComponent}/>
-                <Route path="/reserve2" component={Reserve2}/>
-                <Route path="/reserve3" component={Reserve3}/>
-                <Route path="/mypage" component={MyPage}/>
-            </div>
+                <Theme theme={theme}>
+                    <Route path="/" component={Login} />
+                    <Route path="/index" component={Bar} />
+                    <Route path="/attend" component={AttendComponent} />
+                    <Route path="/score" component={ScoreComponent} />
+                    <Route path="/reserve" component={ReserveComponent}/>
+                    <Route path="/reserve2" component={Reserve2}/>
+                    <Route path="/reserve3" component={Reserve3}/>
+                    <Route path="/mypage" component={MyPage}/>
+                </Theme>
             </Router>
         );
     }
