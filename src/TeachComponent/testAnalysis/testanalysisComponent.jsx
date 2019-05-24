9e699b5dab57c2cd2eaced6926fdd453b868940a
doris0728 <48351016+doris0728@users.noreply.github.com>
2019-05-24 17:43:42 +0800

    analysis

--- src/TeachComponent/testAnalysis/testanalysisComponent.jsx
+++ src/TeachComponent/testAnalysis/testanalysisComponent.jsx
@@ -15,6 +15,12 @@ import ManIcon from '@material-ui/icons/HowToRegRounded'
 import Chart from "react-google-charts";
 import Img from './reading.png'
 import Airtable from 'airtable';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const TABLE_NAME = 'TestScore';
 const base = new Airtable({ apiKey: 'keyA7EKdngjou4Dgy' }).base('appcXtOTPnE4QWIIt');
@@ -42,7 +48,7 @@ const analysisoption = {
 // ];
 const option = {
   colors: ['#5A3DAA', '#FFBF5F', '#ECECEC'],
-  chartArea: { width: '85%' },
+  chartArea: { width: '80%' },
   isStacked: true,
   height: 120,
   width: 950,
@@ -90,12 +96,13 @@ const styles = theme => ({
     fontWeight: "bold",
     color: '#969696',
     fontFamily: "Microsoft JhengHei",
-    letterSpacing: 4,
+    letterSpacing: 3,
   },
   textRight: {
     fontSize: 13,
-    paddingLeft: '65%',
+    //paddingLeft: '65%',
     color: '#FFBF5F',
+    marginLeft: 520
   },
   card: {
     backgroundColor: 'white',
@@ -138,136 +145,148 @@ class EnhancedTable extends React.Component {
     //下面是select跟title的
     age: '',
     classname: '國文第四課',
-    analysisdata0 :[],
-    analysisdata1 :[],
-    analysisdata2 :[],
-    analysisdata3 :[],
-    analysisdata4 :[],
-    analysisdata5 :[],
-    analysisdata6 :[],
-    analysisdata7 :[],
-    analysisdata8 :[],
-    analysisdata9 :[],
-    data:[],
+    myclass: '國文C班',
+    analysisdata0: [],
+    analysisdata1: [],
+    analysisdata2: [],
+    analysisdata3: [],
+    analysisdata4: [],
+    analysisdata5: [],
+    analysisdata6: [],
+    analysisdata7: [],
+    analysisdata8: [],
+    analysisdata9: [],
+    data: [],
+    questionOpen: false,
   };
-  componentDidMount(){
-      const filterSentence = 'AND(test_name ="數講第一冊第二章")';
-      table.select({
+  //
+  question = () => {
+    this.setState({ questionOpen: true });
+    console.log("click")
+  };
+  questionClose = () => {
+    this.setState({ questionOpen: false });
+  };
+  //
+
+  componentDidMount() {
+    const filterSentence = 'AND(test_name ="數講第一冊第二章")';
+    table.select({
       filterByFormula: filterSentence,
       view: "Grid view",
       //maxRecords: 3,
-      }).eachPage((records, fetchNextPage) => {
+    }).eachPage((records, fetchNextPage) => {
 
-        const student_id = records.map((record, index) => record.fields['student_id']);
-        const test_score = records.map((record, index) => record.fields['test_score']);
-        
-        var fail = 0;
-        var good = 0;
-        var absent = 1;
-        for(var index = 0; index < test_score.length; index++) {
-          if(student_id !== "admin"){
-            if(test_score[index] < 60){
-              fail ++;
-            }else{
-              good ++;
-            }
-          }
-        }
-        var tempF = [];
-        tempF.push(['', '及格', '不及格', '缺考'],['', good, fail, absent],);
-        this.setState({data : tempF})
+      const student_id = records.map((record, index) => record.fields['student_id']);
+      const test_score = records.map((record, index) => record.fields['test_score']);
 
-        const Q1 = records.map((record, index) => record.fields['Q1']);
-        const Q2 = records.map((record, index) => record.fields['Q2']);
-        const Q3 = records.map((record, index) => record.fields['Q3']);
-        const Q4 = records.map((record, index) => record.fields['Q4']);
-        const Q5 = records.map((record, index) => record.fields['Q5']);
-        const Q6 = records.map((record, index) => record.fields['Q6']);
-        const Q7 = records.map((record, index) => record.fields['Q7']);
-        const Q8 = records.map((record, index) => record.fields['Q8']);
-        const Q9 = records.map((record, index) => record.fields['Q9']);
-        const Q10 = records.map((record, index) => record.fields['Q10']);
-        var tempQ =[];
-        tempQ.push(Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10);
-        
-        for(var q = 0; q <10 ; q++){
-          var temp=[];
-          var countA = 0;
-          var countB = 0;
-          var countC = 0;
-          var countD = 0;
-          var correct = "";
-          //console.log(tempQ[q]);
-          for(var index = 0; index < student_id.length; index++) {
-            //count answers
-            //Q1
-            if(tempQ[q][index] == "A"){
-              countA ++;
-            }else if(tempQ[q][index] == "B"){
-              countB ++;
-            }else if(tempQ[q][index] == "C"){
-              countC ++;
-            }else if(tempQ[q][index] == "D"){
-              countD ++;
-            }
-            var people = student_id.length - 1;
-            //set correct ans to purple
-            if(student_id[index] == "admin"){
-              correct = tempQ[q][index];
-              if(correct == "A"){
-                temp.push(["", "", { role: "style" }],
-                ["A", (countA-1)/people * 100, "#5A3DAA"],
-                ["B", countB/people * 100, "#FFC880"], 
-                ["C", countC/people * 100, "#FFC880"],
-                ["D", countD/people * 100, "#FFC880"]);
-              }else if(correct == "B"){
-                temp.push(["", "", { role: "style" }],
-                ["A", countA/people * 100, "#FFC880"],
-                ["B", (countB-1)/people * 100, "#5A3DAA"], 
-                ["C", countC/people * 100, "#FFC880"],
-                ["D", countD/people * 100, "#FFC880"]);
-  
-              }else if(correct == "C"){
-                temp.push(["", "", { role: "style" }],
-                ["A", countA/people * 100, "#FFC880"],
-                ["B", countB/people * 100, "#FFC880"], 
-                ["C", countC/people * 100, "#5A3DAA"],
-                ["D", countD/people * 100, "#FFC880"]);
-  
-              }else if(correct == "D"){
-                temp.push(["", "", { role: "style" }],
-                ["A", countA/people * 100, "#FFC880"],
-                ["B", countB/people * 100, "#FFC880"], 
-                ["C", countC/people * 100, "#FFC880"],
-                ["D", (countD-1)/people * 100, "#5A3DAA"]);
-              }
-            }
-          }
-          if(q == 0){
-            this.setState({ analysisdata0 : temp.sort() });
-          }else if(q == 1){
-            this.setState({ analysisdata1 : temp.sort() });
-          }else if(q == 2){
-            this.setState({ analysisdata2 : temp.sort() });
-          }else if(q == 3){
-            this.setState({ analysisdata3 : temp.sort() });
-          }else if(q == 4){
-            this.setState({ analysisdata4 : temp.sort() });
-          }else if(q == 5){
-            this.setState({ analysisdata5 : temp.sort() });
-          }else if(q == 6){
-            this.setState({ analysisdata6 : temp.sort() });
-          }else if(q == 7){
-            this.setState({ analysisdata7 : temp.sort() });
-          }else if(q == 8){
-            this.setState({ analysisdata8 : temp.sort() });
-          }else if(q == 9){
-            this.setState({ analysisdata9 : temp.sort() });
+      var fail = 0;
+      var good = 0;
+      var absent = 1;
+      for (var index = 0; index < test_score.length; index++) {
+        if (student_id !== "admin") {
+          if (test_score[index] < 60) {
+            fail++;
+          } else {
+            good++;
           }
         }
       }
-      );
-    
+      var tempF = [];
+      tempF.push(['', '及格', '不及格', '缺考'], ['', good, fail, absent]);
+      this.setState({ data: tempF })
+
+      const Q1 = records.map((record, index) => record.fields['Q1']);
+      const Q2 = records.map((record, index) => record.fields['Q2']);
+      const Q3 = records.map((record, index) => record.fields['Q3']);
+      const Q4 = records.map((record, index) => record.fields['Q4']);
+      const Q5 = records.map((record, index) => record.fields['Q5']);
+      const Q6 = records.map((record, index) => record.fields['Q6']);
+      const Q7 = records.map((record, index) => record.fields['Q7']);
+      const Q8 = records.map((record, index) => record.fields['Q8']);
+      const Q9 = records.map((record, index) => record.fields['Q9']);
+      const Q10 = records.map((record, index) => record.fields['Q10']);
+      var tempQ = [];
+      tempQ.push(Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10);
+
+      for (var q = 0; q < 10; q++) {
+        var temp = [];
+        var countA = 0;
+        var countB = 0;
+        var countC = 0;
+        var countD = 0;
+        var correct = "";
+        //console.log(tempQ[q]);
+        for (var index = 0; index < student_id.length; index++) {
+          //count answers
+          //Q1
+          if (tempQ[q][index] == "A") {
+            countA++;
+          } else if (tempQ[q][index] == "B") {
+            countB++;
+          } else if (tempQ[q][index] == "C") {
+            countC++;
+          } else if (tempQ[q][index] == "D") {
+            countD++;
+          }
+          var people = student_id.length - 1;
+          //set correct ans to purple
+          if (student_id[index] == "admin") {
+            correct = tempQ[q][index];
+            if (correct == "A") {
+              temp.push(["", "", { role: "style" }],
+                ["A", (countA - 1) / people * 100, "#5A3DAA"],
+                ["B", countB / people * 100, "#FFC880"],
+                ["C", countC / people * 100, "#FFC880"],
+                ["D", countD / people * 100, "#FFC880"]);
+            } else if (correct == "B") {
+              temp.push(["", "", { role: "style" }],
+                ["A", countA / people * 100, "#FFC880"],
+                ["B", (countB - 1) / people * 100, "#5A3DAA"],
+                ["C", countC / people * 100, "#FFC880"],
+                ["D", countD / people * 100, "#FFC880"]);
+
+            } else if (correct == "C") {
+              temp.push(["", "", { role: "style" }],
+                ["A", countA / people * 100, "#FFC880"],
+                ["B", countB / people * 100, "#FFC880"],
+                ["C", countC / people * 100, "#5A3DAA"],
+                ["D", countD / people * 100, "#FFC880"]);
+
+            } else if (correct == "D") {
+              temp.push(["", "", { role: "style" }],
+                ["A", countA / people * 100, "#FFC880"],
+                ["B", countB / people * 100, "#FFC880"],
+                ["C", countC / people * 100, "#FFC880"],
+                ["D", (countD - 1) / people * 100, "#5A3DAA"]);
+            }
+          }
+        }
+        if (q == 0) {
+          this.setState({ analysisdata0: temp.sort() });
+        } else if (q == 1) {
+          this.setState({ analysisdata1: temp.sort() });
+        } else if (q == 2) {
+          this.setState({ analysisdata2: temp.sort() });
+        } else if (q == 3) {
+          this.setState({ analysisdata3: temp.sort() });
+        } else if (q == 4) {
+          this.setState({ analysisdata4: temp.sort() });
+        } else if (q == 5) {
+          this.setState({ analysisdata5: temp.sort() });
+        } else if (q == 6) {
+          this.setState({ analysisdata6: temp.sort() });
+        } else if (q == 7) {
+          this.setState({ analysisdata7: temp.sort() });
+        } else if (q == 8) {
+          this.setState({ analysisdata8: temp.sort() });
+        } else if (q == 9) {
+          this.setState({ analysisdata9: temp.sort() });
+        }
+      }
+    }
+    );
+
   }
 
 
@@ -282,22 +301,73 @@ class EnhancedTable extends React.Component {
 
           <div className={classes.SelectTable}>
             <Typography class={classes.text} nowrap={true}>
-              <a style={{ color: '#FFBF5F', marginLeft: 35 }}>{this.state.classname}</a><a>考試分析</a>
+              <a style={{ marginLeft: 35 }}>
+                <NavLink activeClassName="active" to="/teach/classdetail" style={{ textDecoration: 'none', color: '#818181' }}>
+                  {this.state.myclass}</NavLink>
+                >></a>
+              <NavLink activeClassName="active" to="/teach/classScore" style={{ textDecoration: 'none', color: '#818181' }}><a>考試分析</a></NavLink>>>
+              <a style={{ color: '#FFBF5F' }}>{this.state.classname}</a>
               <a class={classes.textRight}>107學年</a>
             </Typography>
             <Divider variant="middle" />
 
             <Card className={classes.card}>
               <div className={classes.divflex}>
-                <div style={{marginTop:25,marginLeft:15}}><img src={Img} width="70"/></div>
+                <div style={{ marginTop: 20, marginLeft: 15 }}>
+                <Button onClick={this.question}>
+                <img src={Img} width="60"/></Button></div>
                 <div style={{}}><Chart chartType="BarChart" data={this.state.data} options={option} /></div>
 
               </div>
-            </Card>
+              </Card>
 
           </div>
         </div>
+
         {/* select跟title結束 */}
+        <Dialog open={this.state.questionOpen} onClose={this.questionClose} aria-labelledby="form-dialog-title">
+                {/* <DialogTitle id="form-dialog-title">
+                  <a style={{
+                    color: '#5A3DAA', fontFamily: "Microsoft JhengHei", letterSpacing: 4, fontWeight: "bold",
+                    fontSize: 25
+                  }}>更改密碼</a>
+                </DialogTitle> */}
+
+                <DialogContent>
+                  <div style={{display:'flex',height:15}}>
+                  <div style={{width:25,height:15,backgroundColor:'#5A3DAA'}}/>
+                  <a style={{color:'#5A3DAA',fontFamily: "Microsoft JhengHei",letterSpacing:2,fontWeight: "bold",
+              fontSize:12,marginLeft:10}}>及格</a>
+                  <div style={{width:25,height:15,backgroundColor:'#FFBF5F',marginTop:10,marginLeft:15}}/>
+                  <a style={{color:'#FFBF5F',fontFamily: "Microsoft JhengHei",letterSpacing:2,fontWeight: "bold",
+              fontSize:12,marginLeft:10}}>及格</a>
+                  <div style={{width:25,height:15,backgroundColor:'#ECECEC',marginTop:10,marginLeft:15}}/>
+                  <a style={{color:'#ECECEC',fontFamily: "Microsoft JhengHei",letterSpacing:2,fontWeight: "bold",
+              fontSize:12,marginLeft:10}}>及格</a></div>
+                  {/* <DialogContentText>
+                    <a style={{ fontFamily: "Microsoft JhengHei", letterSpacing: 2, fontWeight: "bold" }}>舊密碼</a>
+                  </DialogContentText>
+                  <TextField style={{ marginTop: 10, width: 300 }} autoFocus margin="dense" id="passwd" label="請輸入舊密碼"
+                    type="password" fullWidth variant="outlined" /> */}
+                </DialogContent>
+
+                {/* <DialogContent>
+                  <DialogContentText>
+                    <a style={{ fontFamily: "Microsoft JhengHei", letterSpacing: 2, fontWeight: "bold" }}>更改密碼</a>
+                  </DialogContentText>
+                  <TextField style={{ width: 300 }} margin="dense" id="passwd" label="請輸入新密碼"
+                    type="passwdnew" fullWidth variant="outlined" />
+                </DialogContent>
+                <DialogContent>
+                  <TextField style={{ width: 300 }} margin="dense" id="passwd3" label="再次輸入新密碼"
+                    type="password" fullWidth variant="outlined" />
+                </DialogContent> */}
+
+                <DialogActions>
+                  <Button onClick={this.questionClose} color="primary">
+                    確定 </Button>
+                </DialogActions>
+              </Dialog>
 
         {/* 答題分析開始 */}
         <div className={classes.analysis}>
