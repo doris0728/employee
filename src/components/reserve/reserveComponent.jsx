d768c49383e31b88272e019f6ea268da6a51c4d3
jjtse <vivi999vivi999@gmail.com>
2019-05-30 22:10:20 +0800

    sort

--- src/components/reserve/reserveComponent.jsx
+++ src/components/reserve/reserveComponent.jsx
@@ -235,10 +235,10 @@ class EnhancedTable extends React.Component {
   
     handleRequestSort = (event, property) => {
       const orderBy = property;
-      let order = 'desc';
+      let order = 'asc';
   
-      if (this.state.orderBy === property && this.state.order === 'desc') {
-        order = 'asc';
+      if (this.state.orderBy === property && this.state.order === 'asc') {
+        order = 'dsc';
       }
   
       this.setState({ order, orderBy });
@@ -308,8 +308,9 @@ class EnhancedTable extends React.Component {
      var temp=[];
    
      for(var index = 0; index < count; index++) {
-       temp.push(createData(reserve_date[index],reserve_address[index],reserve_time[index],reserve_people[index]));
-     }
+         temp.push(createData(reserve_date[index],reserve_address[index],reserve_time[index],reserve_people[index]));
+     
+    }
      this.setState({ rows : temp });
      this.setState({dataInit : temp});
      }).catch(err => {
@@ -407,7 +408,7 @@ class EnhancedTable extends React.Component {
             <Table className={classes.table} aria-labelledby="tableTitle">
               <EnhancedTableHead
                 // numSelected={selected.length}
-                order={order}
+                order={order} order={order}
                 orderBy={orderBy}
                 onRequestSort={this.handleRequestSort}
                 //rowCount={data.length}
